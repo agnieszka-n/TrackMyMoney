2dac9c41ae3b62e112ad3fecacdf3242e0aa04f9
Agnieszka <17526941+agnieszka-n@users.noreply.github.com>
2020-01-22 00:07:07 +0100

    Issue #1: Manage categories - Extracted a wrapper for integration test methods. - Added tests for renaming and deleting a category.

--- TrackMyMoney.IntegrationTests/ManageCategoriesTest.cs
+++ TrackMyMoney.IntegrationTests/ManageCategoriesTest.cs
@@ -16,13 +16,101 @@ namespace TrackMyMoney.IntegrationTests
     [TestFixture]
     public class ManageCategoriesTest
     {
+        private const string CONNECTION_STRING = "Data Source=:memory:;Version=3;New=True";
+        private const string LOGGER_FILE_NAME = "TrackMyMoneyTests.log";
+
         [Test]
         public void Can_Add_Category()
         {
-            // Arrange
-            const string CONNECTION_STRING = "Data Source=:memory:;Version=3;New=True";
-            const string LOGGER_FILE_NAME = "TrackMyMoneyTests.log";
+            void Test(IKernel kernel)
+            {
+                // Act
+                var mainVm = kernel.Get<ICostsListViewModel>();
+                mainVm.ShowManageCategoriesCommand.Execute(null);
 
+                var manageCategoriesVm = mainVm.ManageCategoriesViewModel;
+                manageCategoriesVm.ShowAddCommand.Execute(null);
+                manageCategoriesVm.NewCategoryName = "New category";
+                manageCategoriesVm.SaveAddCommand.Execute(null);
+
+                // Assert
+                var manageCategoriesCategories = manageCategoriesVm.Categories;
+                Assert.AreEqual(1, manageCategoriesCategories.Count);
+                Assert.AreEqual("New category", manageCategoriesCategories.First().Name);
+
+                var costsListCategories = mainVm.Categories;
+                Assert.AreEqual(1, costsListCategories.Count);
+                Assert.AreEqual("New category", costsListCategories.First().Name);
+            }
+
+            RunTest(Test);
+        }
+
+        [Test]
+        public void Can_Rename_Category()
+        {
+            void Test(IKernel kernel)
+            {
+                // Act
+                var mainVm = kernel.Get<ICostsListViewModel>();
+                mainVm.ShowManageCategoriesCommand.Execute(null);
+
+                // Add a category
+                var manageCategoriesVm = mainVm.ManageCategoriesViewModel;
+                manageCategoriesVm.ShowAddCommand.Execute(null);
+                manageCategoriesVm.NewCategoryName = "New category";
+                manageCategoriesVm.SaveAddCommand.Execute(null);
+
+                // Rename the category
+                manageCategoriesVm.SelectedCategory = manageCategoriesVm.Categories.First();
+                manageCategoriesVm.ShowRenameCommand.Execute(null);
+                manageCategoriesVm.RenamedCategoryNewName = "Renamed category";
+                manageCategoriesVm.SaveRenameCommand.Execute(null);
+
+                // Assert
+                var manageCategoriesCategories = manageCategoriesVm.Categories;
+                Assert.AreEqual(1, manageCategoriesCategories.Count);
+                Assert.AreEqual("Renamed category", manageCategoriesCategories.First().Name);
+
+                var costsListCategories = mainVm.Categories;
+                Assert.AreEqual(1, costsListCategories.Count);
+                Assert.AreEqual("Renamed category", costsListCategories.First().Name);
+            }
+
+            RunTest(Test);
+        }
+
+        [Test]
+        public void Can_Delete_Category()
+        {
+            void Test(IKernel kernel)
+            {
+                // Act
+                var mainVm = kernel.Get<ICostsListViewModel>();
+                mainVm.ShowManageCategoriesCommand.Execute(null);
+
+                // Add a category
+                var manageCategoriesVm = mainVm.ManageCategoriesViewModel;
+                manageCategoriesVm.ShowAddCommand.Execute(null);
+                manageCategoriesVm.NewCategoryName = "New category";
+                manageCategoriesVm.SaveAddCommand.Execute(null);
+
+                // Delete the category
+                manageCategoriesVm.SelectedCategory = manageCategoriesVm.Categories.First();
+                manageCategoriesVm.ShowDeleteCommand.Execute(null);
+                manageCategoriesVm.ConfirmDeleteCommand.Execute(null);
+
+                // Assert
+                Assert.AreEqual(0, mainVm.Categories.Count);
+                Assert.AreEqual(0, manageCategoriesVm.Categories.Count);
+            }
+
+            RunTest(Test);
+        }
+
+        private void RunTest(Action<IKernel> testMethod)
+        {
+            // Arrange
             IKernel kernel = new StandardKernel();
             var diConfiguration = new NinjectConfiguration();
             diConfiguration.ConfigureKernel(kernel, CONNECTION_STRING);
@@ -37,19 +125,7 @@ namespace TrackMyMoney.IntegrationTests
             {
                 DatabaseInitializer.Initialize(connectionWrapper);
 
-                // Act
-                var mainVm = kernel.Get<ICostsListViewModel>();
-                mainVm.ShowManageCategoriesCommand.Execute(null);
-
-                var manageCategoriesVm = mainVm.ManageCategoriesViewModel;
-                manageCategoriesVm.ShowAddCommand.Execute(null);
-                manageCategoriesVm.NewCategoryName = "New category";
-                manageCategoriesVm.SaveAddCommand.Execute(null);
-
-                // Assert
-                var categories = manageCategoriesVm.Categories;
-                Assert.AreEqual(1, categories.Count);
-                Assert.AreEqual("New category", categories.First().Name);
+                testMethod(kernel);
             }
 
             connection.Dispose();
