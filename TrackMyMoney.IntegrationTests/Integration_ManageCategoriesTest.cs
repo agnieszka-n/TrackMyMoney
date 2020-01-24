c29a4097aefd3be28eb7b01d87af24dbb5d3172f
Agnieszka <17526941+agnieszka-n@users.noreply.github.com>
2020-01-24 22:46:32 +0100

    Issue #1: Manage categories - Added integration tests for preventing adding and renaming a category when its name is already in use.

--- TrackMyMoney.IntegrationTests/Integration_ManageCategoriesTest.cs
+++ TrackMyMoney.IntegrationTests/Integration_ManageCategoriesTest.cs
@@ -94,6 +94,54 @@ namespace TrackMyMoney.IntegrationTests
             RunTest(Test);
         }
 
+        [Test]
+        public void Can_Prevent_Add_Category_When_Name_Duplicated()
+        {
+            void Test(IKernel kernel)
+            {
+                // Arrange
+                var mainVm = kernel.Get<ICostsListViewModel>();
+                var manageCategoriesVm = mainVm.ManageCategoriesViewModel;
+
+                // Act
+                mainVm.ShowManageCategoriesCommand.Execute(null);
+                AddCategory(manageCategoriesVm, "New category");
+                AddCategory(manageCategoriesVm, "New category");
+
+                // Assert
+                Assert.AreEqual(1, mainVm.Categories.Count);
+                Assert.AreEqual("New category", mainVm.Categories.First().Name);
+                var manageCategoriesCategories = manageCategoriesVm.Categories;
+                Assert.AreEqual(1, manageCategoriesCategories.Count);
+                Assert.AreEqual("New category", manageCategoriesCategories.First().Name);
+            }
+
+            RunTest(Test);
+        }
+
+        [Test]
+        public void Can_Prevent_Rename_Category_When_Name_Duplicated()
+        {
+            void Test(IKernel kernel)
+            {
+                // Arrange
+                var mainVm = kernel.Get<ICostsListViewModel>();
+                var manageCategoriesVm = mainVm.ManageCategoriesViewModel;
+
+                // Act
+                mainVm.ShowManageCategoriesCommand.Execute(null);
+                AddCategory(manageCategoriesVm, "Category 1");
+                AddCategory(manageCategoriesVm, "Category 2");
+                RenameCategory(manageCategoriesVm, manageCategoriesVm.Categories.First(), "Category 2");
+
+                // Assert
+                Assert.AreEqual("Category 1", mainVm.Categories.First().Name);
+                Assert.AreEqual("Category 1", manageCategoriesVm.Categories.First().Name);
+            }
+
+            RunTest(Test);
+        }
+
         private void RunTest(Action<IKernel> testMethod)
         {
             IKernel kernel = new StandardKernel();
