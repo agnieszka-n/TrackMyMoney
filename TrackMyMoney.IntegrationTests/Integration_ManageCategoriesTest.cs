3e3c1e1d0810e79f5fa9b47b3adbe5f918f52efe
Agnieszka <17526941+agnieszka-n@users.noreply.github.com>
2020-03-16 22:32:51 +0100

    Issue #2: Manage costs - Extracted common integration tests elements to a base class.

--- TrackMyMoney.IntegrationTests/Integration_ManageCategoriesTest.cs
+++ TrackMyMoney.IntegrationTests/Integration_ManageCategoriesTest.cs
@@ -1,24 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Ninject;
 using NUnit.Framework;
-using TrackMyMoney.Main;
-using TrackMyMoney.Services.Contracts.Database;
-using TrackMyMoney.Services.Database;
 using TrackMyMoney.ViewModels.Contracts;
 
 namespace TrackMyMoney.IntegrationTests
 {
     [TestFixture]
-    public class Integration_ManageCategoriesTest
+    public class Integration_ManageCategoriesTest: TextFixtureBase
     {
-        private const string CONNECTION_STRING = "Data Source=:memory:;Version=3;New=True";
-        private const string LOGGER_FILE_NAME = "TrackMyMoneyTests.log";
-
         [Test]
         public void Can_Add_Category()
         {
@@ -140,28 +133,6 @@ namespace TrackMyMoney.IntegrationTests
             }
 
             RunTest(Test);
-        }
-
-        private void RunTest(Action<IKernel> testMethod)
-        {
-            IKernel kernel = new StandardKernel();
-            var diConfiguration = new NinjectConfiguration();
-            diConfiguration.ConfigureKernel(kernel, CONNECTION_STRING);
-
-            var connection = new SQLiteConnection(CONNECTION_STRING);
-            var dbProxy = new DatabaseProxy(connection);
-            kernel.Rebind<IDatabaseProxy>().ToConstant(dbProxy);
-
-            LoggerInitalizer.InitializeFileLogger(LOGGER_FILE_NAME);
-
-            using (var connectionWrapper = dbProxy.CreateConnectionWrapper())
-            {
-                DatabaseInitializer.Initialize(connectionWrapper);
-
-                testMethod(kernel);
-            }
-
-            connection.Dispose();
         }
 
         private static void AddCategory(IManageCategoriesViewModel manageCategoriesVm, string categoryName)
---
+++ TrackMyMoney.IntegrationTests/TextFixtureBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+using TrackMyMoney.Main;
+using TrackMyMoney.Services.Contracts.Database;
+using TrackMyMoney.Services.Database;
+
+namespace TrackMyMoney.IntegrationTests
+{
+    public abstract class TextFixtureBase
+    {
+        protected const string CONNECTION_STRING = "Data Source=:memory:;Version=3;New=True";
+        protected const string LOGGER_FILE_NAME = "TrackMyMoneyTests.log";
+
+        protected void RunTest(Action<IKernel> testMethod)
+        {
+            IKernel kernel = new StandardKernel();
+            var diConfiguration = new NinjectConfiguration();
+            diConfiguration.ConfigureKernel(kernel, CONNECTION_STRING);
+
+            var connection = new SQLiteConnection(CONNECTION_STRING);
+            var dbProxy = new DatabaseProxy(connection);
+            kernel.Rebind<IDatabaseProxy>().ToConstant(dbProxy);
+
+            LoggerInitalizer.InitializeFileLogger(LOGGER_FILE_NAME);
+
+            using (var connectionWrapper = dbProxy.CreateConnectionWrapper())
+            {
+                DatabaseInitializer.Initialize(connectionWrapper);
+
+                testMethod(kernel);
+            }
+
+            connection.Dispose();
+        }
+    }
+}
